179df2f95a8fa494aedbabfc02ba7ba168794098
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2018-04-22 09:51:24 -0300

    Initial Commit

---
+++ NavigationSwipeCustom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationSwipeCustom
+//
+//  Created by Michel Anderson Lutz Teixeira on 22/04/2018.
+//  Copyright © 2018 Masters Mx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
